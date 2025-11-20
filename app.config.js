df8f12d6777d696e165620f9ca9c716b9046d627
Hasan Aden <kamaaloos/gmail.com>
2025-11-20 07:29:27 +0200

    Prepare build

--- app.config.js
+++ app.config.js
@@ -18,8 +18,8 @@ export default {
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       supabaseServiceKey: process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY,
       projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-      eas: {
-        projectId: process.env.EXPO_PUBLIC_PROJECT_ID
+      "eas": {
+        "projectId": "1d6eff74-12ce-45fd-8491-c0e1e3fb6ed4"
       }
     }
   }
